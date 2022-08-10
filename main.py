965662f1b859df7d280134c12ec6b4de9aff2497
Kavyan Patel <kavyanpatel1104@gmail.com>
2022-08-09 22:35:22 -0700

    Initialize Update
    Added the basics to create a screen, created a circle from the tutorial

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("Hello Kavyan")
+import pygame
+
+#initialize pygame
+pygame.init()
+
+#creating the screen (500 pixels X 500 pixels)
+screen = pygame.display.set_mode([500,500])
+
+#create game loop condition
+running = True
+while running: 
+
+    #check to see if exit condition is met
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:#if user trys to exit
+            running = False
+    
+    screen.fill((255,255,255))#fill screen with color (RGB values)
+
+    pygame.draw.circle(screen, #screen name
+    (0,0,255), #rgb color
+    (250,250), #center coordinate for circle
+    75)#radius
+
+    #update the display
+    pygame.display.flip()
+
+pygame.quit()
