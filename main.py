4d64302924eb9a27cd6a42892cb8c81dba1a36a2
Kavyan Patel <kavyanpatel1104@gmail.com>
2022-08-18 20:40:18 -0700

    Movement Update #2
    - Coast Movement for sprite
    
    - Random Meteor Generation on the X axis
    
    - Movement of generated meteor down
    
    TODO:
    
    Only one meteor is generated, multiple at the same time should be generated
           - Later we can make them random
              sized
    
    Crash Detection is needed
    
    better GUI
       - Pause screen, Home screen,
          settings, etc.

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 # Import the pygame module
 import pygame
+import random
+import threading
+import os
+
+clock = pygame.time.Clock()
 
 # Import pygame.locals for easier access to key coordinates
-# Updated to conform to flake8 and black standards
 from pygame.locals import (
     K_UP,
     K_DOWN,
@@ -10,6 +14,7 @@ from pygame.locals import (
     K_RIGHT,
     K_ESCAPE,
     KEYDOWN,
+    KEYUP,
     QUIT,
     K_a,
     K_d, 
@@ -19,8 +24,12 @@ from pygame.locals import (
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 667
 
+RANDOM_X = random.randint(20, 980)
+
 velocity = 12
 
+meteor = pygame.Surface((50,50))
+meteor.fill((110, 38, 14))
 bg = pygame.image.load("space.jpeg")
 
 sprite = pygame.image.load("rocket.jpeg")
@@ -28,7 +37,7 @@ sprite = pygame.transform.scale(sprite, (50, 50))
 
 # Define a player object by extending pygame.sprite.Sprite
 # The surface drawn on the screen is now an attribute of 'player'
-
+ 
 # Initialize pygame
 pygame.init()
 
@@ -37,15 +46,37 @@ pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 x = SCREEN_WIDTH/2
-y = SCREEN_HEIGHT/2
+y = SCREEN_HEIGHT-100
 
 # Variable to keep the main loop running
 running = True
 
 # Main loop
+
+x_change = 0
+meteor_change = 0
+m = 0
+c = 0
+
+def meteor_rain(x):
+    while x:
+        r = random.randint(20, 980)
+        screen.blit(meteor, (r, m))
+        if c%97:
+            meteor_change = 5
+        else:
+            meteor_change = 0 
+    
+        if c == 97:
+            c = 0
+    
+    m+=meteor_change
+    c+=1
+
 while running:
     screen.blit(bg, (0, 0))
     screen.blit(sprite, (x, y))
+    screen.blit(meteor, (RANDOM_X, m))
 
     # for loop through the event queue
     for event in pygame.event.get():
@@ -54,20 +85,38 @@ while running:
             # If the Esc key is pressed, then exit the main loop
             if event.key == K_ESCAPE:
                 running = False
-            if event.key == pygame.K_a:
-                x-=velocity
-            if event.key == pygame.K_d:
-                x+=velocity
+            # If the 'a' key is pressed: Move the sprite left:
+            if event.key == K_a:
+                x_change = -12 
+            # If the 'd' key is pressed: Move the sprite left:
+            if event.key == K_d:
+                x_change = 12
+        if event.type == KEYUP:
+            if event.key == K_a or event.key == K_d:
+                x_change = 0
         # Check for QUIT event. If QUIT, then set running to false.
         elif event.type == QUIT:
             running = False
-
-
     
+    x+=x_change
 
-    # Draw the player on the screen
-
-
+    if x<0:
+        x=0
+    elif x>950:
+        x = 950
+    
+    if c%97:
+        meteor_change = 5
+    else:
+        meteor_change = 0 
+    
+    if c == 97:
+        c = 0
+    
+    m+=meteor_change
+    c+=1
     # Update the display
     pygame.display.update()
+    
+    clock.tick(40)
 
